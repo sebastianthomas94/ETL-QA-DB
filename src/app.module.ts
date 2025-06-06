4402352e81ceb48c0b9c16f8abcc8f944142f473
as-harudeen <asharudeenalikt@gmail.com>
2025-06-06 12:03:40 +0530

    refactor: refine throttler config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,20 @@
 import { Module } from "@nestjs/common";
 import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
-import { THROTTLER_CONFIG } from "./common/config/throttler.config";
 import { LoggerModule } from "nestjs-pino";
 import { pinoConfig } from "@common/config/pino.config";
 import { RouteModule } from "./route.module";
 import { APP_GUARD } from "@nestjs/core";
 import { GlobalModule } from "@common/global/global.module";
 import { EnvironmentService } from "@common/global/environment.service";
+import { throttlerConfig } from "@common/config/throttler.config";
 
 @Module({
     imports: [
         GlobalModule,
-        ThrottlerModule.forRoot(THROTTLER_CONFIG),
+        ThrottlerModule.forRootAsync({
+            useFactory: (envService: EnvironmentService) => throttlerConfig(envService),
+            inject: [EnvironmentService],
+        }),
         LoggerModule.forRootAsync({
             useFactory: (envService) => pinoConfig(envService),
             inject: [EnvironmentService],
--- src/common/config/throttler.config.ts
+++ src/common/config/throttler.config.ts
@@ -1,8 +1,11 @@
-export const THROTTLER_CONFIG = [
+import { EnvironmentService } from "@common/global/environment.service";
+import { ThrottlerModuleOptions } from "@nestjs/throttler";
+
+export const throttlerConfig = (envService: EnvironmentService): ThrottlerModuleOptions => [
     {
         name: "short",
         ttl: 1000,
-        limit: 3,
+        limit: envService.isProduction ? 3 : 10, // 3 requests per second in production, 10 in development
     },
     {
         name: "long",
