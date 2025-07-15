0edb5e958889363da4ea093f978e10b2dad1a2f2
sebastianthomas94 <53168297+sebastianthomas94@users.noreply.github.com>
2025-07-15 23:01:08 +0530

    feat: env init

--- src/common/global/environment.service.ts
+++ src/common/global/environment.service.ts
@@ -3,7 +3,8 @@ import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class EnvironmentService {
-    constructor(private configService: ConfigService) {}
+    constructor(private readonly configService: ConfigService) {}
+
     get isDevelopment() {
         return this.configService.get("NODE_ENV", { infer: true }) === "development";
     }
@@ -16,6 +17,46 @@ export class EnvironmentService {
         return {
             port: this.configService.get("PORT", { infer: true })!,
             // host: this.configService.get("HOST", { infer: true })!,
+        };
+    }
+
+    get mongoCollectionNames() {
+        return this.configService.get("MONGO_COLLECTION_NAMES", { infer: true }) || [];
+    }
+
+    get pgTableNames() {
+        return this.configService.get("PG_TABLE_NAMES", { infer: true }) || [];
+    }
+
+    get productionMongo() {
+        return {
+            uri: this.configService.get("PROD_MONGO_URI", { infer: true })!,
+        };
+    }
+
+    get productionPostgres() {
+        return {
+            host: this.configService.get("PROD_PG_HOST", { infer: true })!,
+            port: this.configService.get("PROD_PG_PORT", { infer: true })!,
+            database: this.configService.get("PROD_PG_DB", { infer: true })!,
+            username: this.configService.get("PROD_PG_USER", { infer: true })!,
+            password: this.configService.get("PROD_PG_PASS", { infer: true })!,
+        };
+    }
+
+    get qaMongo() {
+        return {
+            uri: this.configService.get("QA_MONGO_URI", { infer: true })!,
+        };
+    }
+
+    get qaPostgres() {
+        return {
+            host: this.configService.get("QA_PG_HOST", { infer: true })!,
+            port: this.configService.get("QA_PG_PORT", { infer: true })!,
+            database: this.configService.get("QA_PG_DB", { infer: true })!,
+            username: this.configService.get("QA_PG_USER", { infer: true })!,
+            password: this.configService.get("QA_PG_PASS", { infer: true })!,
         };
     }
 }
